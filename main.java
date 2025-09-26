9d96ce6b72f359506f8b5c0120b6edbb3bcd8b89
FELIPE TAVARES DIAS <felipe.dias003@edu.pucrs.br>
2025-09-26 18:30:18 -0300

    Cria main inicial com print

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+
+    public static void main(String[] args){
+    System.out.print("Olá, este é um repositório criado para o trabalho de GCS por Felipe e Eduardo.");
+    }
+
+}
